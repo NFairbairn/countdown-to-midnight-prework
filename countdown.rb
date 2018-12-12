15e597097e9252c7aaf7c319dde4dfc57c95fd3b
Noah Fairbairn <noahfairbairn@gmail.com>
2018-12-12 23:36:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  time_seconds = 10
+  while time_seconds > 0
+    puts "#{time_seconds} SEOND(S)"
+    break if time_seconds == 0
+    puts "HAPPY NEW YEAR!"
+    time_seconds -= 1
+  end
 end
